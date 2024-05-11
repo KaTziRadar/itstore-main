adb9ad434847694f9cb47648b127c758138916f7
KaTziRadar <adarkatzir@gmail.com>
2024-05-11 13:33:04 +0300

    try firefox

--- frontend/test/NavBar.spec.js
+++ frontend/test/NavBar.spec.js
@@ -7,7 +7,7 @@ describe("NavBar test", function() {
     let driver;
 
     beforeEach(async function() {
-        driver = await new Builder().forBrowser(Browser.CHROME).build();
+        driver = await new Builder().forBrowser(Browser.FIREFOX).build();
     });
 
     afterEach(async function() {
