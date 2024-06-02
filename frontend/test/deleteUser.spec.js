e4c06b20e2dbca7092c5a88440b78a9bb9f0733d
hazanido <hazanido@gmail.com>
2024-06-02 11:29:26 +0300

    fix 5

--- frontend/test/deleteUser.spec.js
+++ frontend/test/deleteUser.spec.js
@@ -3,15 +3,15 @@ const { Builder, By, until, Browser } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 describe("Delete user test", function() {
-    this.timeout(30000); // Set a longer timeout
+    this.timeout(100000); // Increase timeout to 60 seconds
 
     let driver;
 
     beforeEach(async function() {
         let options = new chrome.Options();
-        options.addArguments('--headless'); // Run in headless mode
-        options.addArguments('--no-sandbox'); // Needed if running as root
-        options.addArguments('--disable-dev-shm-usage'); // Overcome limited resource problems
+        options.addArguments('--headless');
+        options.addArguments('--no-sandbox');
+        options.addArguments('--disable-dev-shm-usage');
 
         driver = await new Builder()
             .forBrowser(Browser.CHROME)
@@ -25,22 +25,20 @@ describe("Delete user test", function() {
         }
     });
 
-    it("delete user successfully", function(done) { // Add 'done' as parameter
+    it("delete user successfully", function(done) {
         (async () => {
             await driver.get('https://itstore-main-fe-omj2.onrender.com/');
 
             await driver.findElement(By.linkText('Register')).click();
-            await driver.findElement((By.id('email'))).sendKeys('test@test.test');
-            await driver.findElement((By.id('password'))).sendKeys('test123');
-            await driver.findElement((By.id('cnfrm-password'))).sendKeys('test123');
+            await driver.findElement(By.id('email')).sendKeys('test@test.test');
+            await driver.findElement(By.id('password')).sendKeys('test123');
+            await driver.findElement(By.id('cnfrm-password')).sendKeys('test123');
 
             const registerButton = await driver.wait(until.elementLocated(By.className('btn')));
             await registerButton.click();
 
             // Wait for the alert to appear
             await driver.wait(until.alertIsPresent());
-
-            // Switch to the alert and accept it
             let alert = await driver.switchTo().alert();
             await alert.accept();
 
@@ -51,16 +49,11 @@ describe("Delete user test", function() {
 
             // Wait for the alert to appear
             await driver.wait(until.alertIsPresent());
-
-            // Switch to the alert and accept it
             alert = await driver.switchTo().alert();
             await alert.accept();
 
             // Press the delete user button 
-            await driver.sleep(1000)
             await driver.findElement(By.linkText('Delete User')).click();
-            await driver.sleep(1000);
-
             // Wait for the alert to appear
             await driver.wait(until.alertIsPresent());
             alert = await driver.switchTo().alert();
@@ -68,8 +61,8 @@ describe("Delete user test", function() {
             await alert.accept();
 
             assert.equal(msg, 'User deleted successfully!', 'Expected error does not match actual error');
-            done(); // Call 'done()' to indicate test completion
-        })().catch(done); // Catch any errors and call 'done()' to indicate test completion
+            done();
+        })().catch(done);
     });
 
 });
