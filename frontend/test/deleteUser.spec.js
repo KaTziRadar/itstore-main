ab7fbff9f75acdb45df52c1c2bfc51b8687729b4
hazanido <hazanido@gmail.com>
2024-06-02 11:46:24 +0300

    test3

--- frontend/test/deleteUser.spec.js
+++ frontend/test/deleteUser.spec.js
@@ -3,15 +3,15 @@ const { Builder, By, until, Browser } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 describe("Delete user test", function() {
-    this.timeout(100000); // Increase timeout to 60 seconds
+    this.timeout(30000); // Set a longer timeout
 
     let driver;
 
     beforeEach(async function() {
         let options = new chrome.Options();
-        options.addArguments('--headless');
-        options.addArguments('--no-sandbox');
-        options.addArguments('--disable-dev-shm-usage');
+        options.addArguments('--headless'); // Run in headless mode
+        options.addArguments('--no-sandbox'); // Needed if running as root
+        options.addArguments('--disable-dev-shm-usage'); // Overcome limited resource problems
 
         driver = await new Builder()
             .forBrowser(Browser.CHROME)
@@ -27,43 +27,48 @@ describe("Delete user test", function() {
 
     it("delete user successfully", async function() {
         await driver.get('https://itstore-main-fe-omj2.onrender.com/');
-    
+
         await driver.findElement(By.linkText('Register')).click();
-        await driver.findElement(By.id('email')).sendKeys('test@test.test');
-        await driver.findElement(By.id('password')).sendKeys('test123');
-        await driver.findElement(By.id('cnfrm-password')).sendKeys('test123');
-    
-        const registerButton = await driver.wait(until.elementLocated(By.className('btn')));
-        await registerButton.click();
-    
+        await driver.findElement((By.id('email'))).sendKeys('test@test.test');
+        await driver.findElement((By.id('password'))).sendKeys('test123');
+        await driver.findElement((By.id('cnfrm-password'))).sendKeys('test123');
+       
+
+       const registerButton = await driver.wait(until.elementLocated(By.className('btn')));
+       await registerButton.click();
+
         // Wait for the alert to appear
         await driver.wait(until.alertIsPresent());
+
+        // Switch to the alert and accept it
         let alert = await driver.switchTo().alert();
         await alert.accept();
-    
-        // Login for the user we want to delete 
+        ///////////////////////////////////////////////////
+
+        //Login for the user we want to delete 
         await driver.findElement(By.id('email')).sendKeys('test@test.test');
         await driver.findElement(By.id('password')).sendKeys('test123');
-        await driver.findElement(By.css('.btn')).click();
-    
+        await driver.findElement(By.css('.btn')).click();       
         // Wait for the alert to appear
         await driver.wait(until.alertIsPresent());
-        alert = await driver.switchTo().alert();
+        // Switch to the alert and accept it
+         alert = await driver.switchTo().alert();
         await alert.accept();
-    
-        // Press the delete user button 
-        await driver.findElement(By.linkText('Delete User')).click();
-    
-        // Wait for the alert to appear
-        const msg = await driver.wait(until.alertIsPresent()).then(async () => {
-            alert = await driver.switchTo().alert();
-            const alertText = await alert.getText();
-            await alert.accept();
-            return alertText;
-        });
-    
-        assert.equal(msg, 'User deleted successfully!', 'Expected error does not match actual error');
-    });
-    
+        //////////////////////////////////////////////////////////////
 
-});
+
+        //Press the delete user button 
+        await driver.sleep(1000)
+        await driver.findElement(By.linkText('Delete User')).click();
+        await driver.sleep(1000);
+        
+         // Wait for the alert to appear
+        await driver.wait(until.alertIsPresent());
+        alert = await driver.switchTo().alert();
+        const msg = await alert.getText();
+        await alert.accept();
+        assert.equal(msg, 'User deleted successfully!', 'Expected error does not match actual error');
+
+   });
+
+}); 
